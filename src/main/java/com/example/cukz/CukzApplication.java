1995f1e68c97f0ffc69d832c7f5786e8504afdfb
Kim Jihyun <legojhk@gmail.com>
2024-03-04 21:39:11 +0900

    init commit

---
+++ src/main/java/com/example/cukz/CukzApplication.java
@@ -0,0 +1,13 @@
+package com.example.cukz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CukzApplication.class, args);
+    }
+
+}
