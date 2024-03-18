cec3b2d92b016535c044fe30911bf0457006a3d5
YerimSW <swyr1016@gmail.com>
2024-03-18 22:00:42 +0900

    feat: [#6] 아이디, 비밀번호, 닉네임 유효성 검증

--- src/main/java/com/example/member/controller/MemberController.java
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.example.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/register")
-    public ResponseEntity<MemberRegisterDto> registerMember(@RequestBody MemberRegisterDto memberRegisterDto) {
+    public ResponseEntity<MemberRegisterDto> registerMember(@RequestBody @Validated MemberRegisterDto memberRegisterDto) {
         memberService.registerMember(memberRegisterDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
---
+++ src/test/java/com/example/member/controller/MemberControllerTest.java
@@ -0,0 +1,143 @@
+package com.example.member.controller;
+
+import com.example.member.dto.MemberRegisterDto;
+import com.example.member.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+class MemberControllerTest {
+
+    @MockBean
+    private MemberController memberController;
+
+    @Mock
+    private MemberService memberService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    @DisplayName("로그인 DTO 정상 요청")
+    void registerOKMember() throws Exception {
+        // given
+        MemberRegisterDto dto = MemberRegisterDto.builder()
+                .username("username_1234")
+                .password("password!1234")
+                .nickname("닉네임")
+                .build();
+
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/members/register")
+                        .content(objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        MvcResult mvcResult = resultActions
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    @DisplayName("로그인 DTO username 비정상 요청")
+    void registerUsernameNotOKMember() throws Exception {
+        // given
+        MemberRegisterDto dto = MemberRegisterDto.builder()
+                .username("_username")
+                .password("password!1234")
+                .nickname("닉네임")
+                .build();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/members/register")
+                        .content(objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        MvcResult mvcResult = resultActions
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    @DisplayName("로그인 DTO password 비정상 요청")
+    void registerPasswordNotOKMember() throws Exception {
+        // given
+        MemberRegisterDto dto = MemberRegisterDto.builder()
+                .username("username_1234")
+                .password("pass4")
+                .nickname("닉네임")
+                .build();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/members/register")
+                        .content(objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        MvcResult mvcResult = resultActions
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    @DisplayName("로그인 DTO nickname 비정상 요청")
+    void registerNicknameNotOKMember() throws Exception {
+        // given
+        MemberRegisterDto dto = MemberRegisterDto.builder()
+                .username("username_1234")
+                .password("password!1234")
+                .nickname(" ")
+                .build();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/members/register")
+                        .content(objectMapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        MvcResult mvcResult = resultActions
+                .andExpect(status().is4xxClientError())
+                .andDo(print())
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+}
