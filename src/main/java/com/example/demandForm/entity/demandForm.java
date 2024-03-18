c1e61b304b45ca0816bd008f8129fdd741fc43ce
Kim Jihyun <legojhk@gmail.com>
2024-03-18 09:41:28 +0900

    feat : demand form, purchase form 엔티티 설계

---
+++ src/main/java/com/example/demandForm/entity/demandForm.java
@@ -0,0 +1,32 @@
+package com.example.demandForm.entity;
+
+import com.example.member.entity.Member;
+import com.example.product.entity.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class demandForm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
