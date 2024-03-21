449c7fac1dc93ed770c0c850d3cad31d743856ee
Kim Jihyun <legojhk@gmail.com>
2024-03-21 13:43:33 +0900

    feat(form) : 수요조사 참여 API - 참여 날짜 확인 로직 추가

--- src/main/java/com/example/demandForm/service/DemandFormService.java
+++ src/main/java/com/example/demandForm/service/DemandFormService.java
@@ -8,6 +8,7 @@ import com.example.member.entity.Member;
 import com.example.product.entity.Product;
 import com.example.product.repository.ProductRepository;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,8 @@ public class DemandFormService {
     @Transactional
     public DemandFormResponseDto demandMember(Long productId, DemandFormRequestDto requestDto, Member member) {
 
+        // to-do : member role 검증
+
         Optional<DemandForm> demandFormExist = demandFormRepository.findByProductIdAndMemberId(
             productId, member.getId());
         if (demandFormExist.isPresent()) {
@@ -32,6 +35,8 @@ public class DemandFormService {
         }
 
         Product product = findProduct(productId);
+        checkPeriod(product);
+
         DemandForm demandForm = DemandForm.toMemberEntity(member, product, requestDto);
         demandFormRepository.save(demandForm);
 
@@ -42,17 +47,13 @@ public class DemandFormService {
     public DemandFormResponseDto demandNonMember(Long productId, DemandFormRequestDto requestDto) {
 
         Product product = findProduct(productId);
+        checkPeriod(product);
+
         long orderNumber = generateOrderNumber();
         DemandForm demandForm = DemandForm.toNonMemberEntity(orderNumber, product, requestDto);
         demandFormRepository.save(demandForm);
 
         return DemandFormResponseDto.toResponseDto(demandForm);
-    }
-
-    private Product findProduct(Long productId) {
-        return productRepository.findById(productId).orElseThrow(() ->
-            new IllegalArgumentException("상품을 찾을 수 없습니다.")
-        );
     }
 
     public long generateOrderNumber() {
@@ -66,5 +67,18 @@ public class DemandFormService {
 
         String orderNumberStr = dateString + String.format("%010d", randomNumber);
         return Long.parseLong(orderNumberStr);
+    }
+
+    private Product findProduct(Long productId) {
+        return productRepository.findById(productId).orElseThrow(() ->
+                new IllegalArgumentException("상품을 찾을 수 없습니다.")
+        );
+    }
+
+    public void checkPeriod(Product product){
+        LocalDateTime now = LocalDateTime.now();
+        if(now.isAfter(product.getEndDate()) || now.isBefore(product.getStartDate())) {
+            throw new IllegalArgumentException("수요조사 참여 가능 기간이 아닙니다.");
+        }
     }
 }
