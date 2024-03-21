3009003bcae89eed7cd27710a665bf64c026a322
Kim Jihyun <legojhk@gmail.com>
2024-03-21 17:08:08 +0900

    refactor(form): requestDto validation 추가

--- src/main/java/com/example/demandForm/dto/DemandFormRequestDto.java
+++ src/main/java/com/example/demandForm/dto/DemandFormRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.demandForm.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,5 +8,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public class DemandFormRequestDto {
 
+    @Min(value = 1, message = "수량은 1개 이상 입력해주세요")
     private int quantity;
 }
