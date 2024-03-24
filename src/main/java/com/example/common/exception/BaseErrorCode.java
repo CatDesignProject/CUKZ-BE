1943d4cbf584ab0e85c9905c28ff46abfe80cffd
veronees <jsg6854@naver.com>
2024-03-24 20:20:45 +0900

    other: [#7] BaseErrorCode 수정

--- src/main/java/com/example/common/exception/BaseErrorCode.java
+++ src/main/java/com/example/common/exception/BaseErrorCode.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.*;
-
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 @Getter
 @RequiredArgsConstructor
 public enum BaseErrorCode {
