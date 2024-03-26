1cc9d64a28bd5842503fe9072e7c1b1b1b9b5e09
veronees <jsg6854@naver.com>
2024-03-26 18:45:21 +0900

    bugfix: [#23] dev 코드 수정

--- src/main/java/com/example/product_image/controller/ProductImageController.java
+++ src/main/java/com/example/product_image/controller/ProductImageController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-//@RestController
+@RestController
 @RequiredArgsConstructor
 public class ProductImageController {
 
