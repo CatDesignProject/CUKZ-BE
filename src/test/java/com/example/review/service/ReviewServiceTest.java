683f0eca2bad812d06f16361b340b25d762a5be0
Kim Jihyun <legojhk@gmail.com>
2024-04-07 22:05:34 +0900

    bugfix : test 오류 수정

--- src/test/java/com/example/review/service/ReviewServiceTest.java
+++ src/test/java/com/example/review/service/ReviewServiceTest.java
@@ -43,7 +43,7 @@ class ReviewServiceTest {
     @InjectMocks
     ReviewService reviewService;
 
-    PurchaseForm purchaseForm =ReviewTestBuilder.testPurchaseForm();
+    PurchaseForm purchaseForm = ReviewTestBuilder.testPurchaseForm();
     ReviewRequestDto reviewRequestDto = ReviewTestBuilder.testReviewRequestDto();
     Member buyer = purchaseForm.getMember();
     Member seller = ReviewTestBuilder.testSeller();
@@ -63,6 +63,7 @@ class ReviewServiceTest {
                 given(memberRepository.findById(seller.getId())).willReturn(Optional.of(seller));
                 given(reviewRepository.save(any(Review.class))).willReturn(any(Review.class));
             }
+
             @Test
             @DisplayName("리뷰 작성에 성공한다.")
             void it_returns_succes_save_review() {
@@ -73,6 +74,7 @@ class ReviewServiceTest {
                 then(reviewRepository).should(times(1)).save(any(Review.class));
             }
         }
+
         @Nested
         @DisplayName("이미 해당 구매폼이 리뷰를 작성한 경우")
         class Context_with_already_write_from_purchaseFormId {
@@ -83,6 +85,7 @@ class ReviewServiceTest {
                 given(purchaseFormRepository.findById(purchaseForm.getId())).willReturn(Optional.of(purchaseForm));
                 given(reviewRepository.findByPurchaseFormId(purchaseForm.getId())).willReturn(Optional.of(review));
             }
+
             @Test
             @DisplayName("이미 리뷰를 작성했다는 예외를 발생시킨다.")
             void it_returns_already_write_review() {
@@ -91,6 +94,7 @@ class ReviewServiceTest {
                         .hasMessage("이미 리뷰를 작성했습니다.");
             }
         }
+
         @Nested
         @DisplayName("해당 구매폼이 sellerId 총대가 공구를 진행한 물품이 아닌 경우")
         class Context_with_purchaseForm_not_match_seller {
@@ -99,6 +103,7 @@ class ReviewServiceTest {
                 given(reviewRepository.findByPurchaseFormId(purchaseForm.getId())).willReturn(Optional.empty());
                 given(purchaseFormRepository.findById(purchaseForm.getId())).willReturn(Optional.of(purchaseForm));
             }
+
             @Test
             @DisplayName("해당 총대에게 리뷰를 작성할 권한 없음 예외를 발생시킨다.")
             void it_returns_unauthorized_write_review_to_seller() {
@@ -107,6 +112,7 @@ class ReviewServiceTest {
                         .hasMessage("해당 총대에게 리뷰를 작성할 권한이 없습니다.");
             }
         }
+
         @Nested
         @DisplayName("구매폼이 없는 경우")
         class Context_with_not_exist_purchaseForm {
@@ -114,14 +120,16 @@ class ReviewServiceTest {
             void setUp() {
                 given(purchaseFormRepository.findById(1L)).willReturn(Optional.empty());
             }
+
             @Test
             @DisplayName("해당 구매폼을 찾을 수 없다는 예외를 발생시킨다.")
             void it_returns_not_found_purchase_form() {
                 assertThatThrownBy(() -> reviewService.saveReview(reviewRequestDto, seller.getId(), purchaseForm.getId(), buyer.getId()))
                         .isInstanceOf(GlobalException.class)
-                        .hasMessage("해당 구매폼을 찾을 수 없습니다.");
+                        .hasMessage("해당 폼을 찾을 수 없습니다.");
             }
         }
+
         @Nested
         @DisplayName("공구가 완료되지 않아서 아직 리뷰 작성 기간이 아닌 경우")
         class Context_with_sale_status_not_complete {
@@ -130,6 +138,7 @@ class ReviewServiceTest {
                 purchaseForm = ReviewTestBuilder.testPurchaseForm2();
                 given(purchaseFormRepository.findById(purchaseForm.getId())).willReturn(Optional.of(purchaseForm));
             }
+
             @Test
             @DisplayName("아직 리뷰 작성이 불가능한 상품 예외를 발생시킨다.")
             void it_returns_not_yet_write_review() {
@@ -138,15 +147,18 @@ class ReviewServiceTest {
                         .hasMessage("아직 해당 상품은 리뷰 작성을 할 수 없습니다.");
             }
         }
+
         @Nested
         @DisplayName("로그인한 유저가 구매폼을 작성한 유저가 아닌 경우")
         class Context_with_login_user_not_match_purchase_form_user {
             Member loginMember;
+
             @BeforeEach
             void setUp() {
                 loginMember = ProductTestBuilder.testMember2Build(); //로그인 유저, memberId 2L
                 given(purchaseFormRepository.findById(purchaseForm.getId())).willReturn(Optional.of(purchaseForm));
             }
+
             @Test
             @DisplayName("해당 구매폼으로 리뷰를 작성할 권한이 없음 예외를 발생시킨다.")
             void it_returns_un_authorized_write_review_from_purchase_form() {
@@ -164,6 +176,7 @@ class ReviewServiceTest {
         @DisplayName("유효한 총대의 sellerId 값이 주어진 경우")
         class Context_with_seller_id {
             ReviewResponseDto reviewResponseDto;
+
             @BeforeEach
             void setUp() {
                 given(memberRepository.findById(seller.getId())).willReturn(Optional.of(seller));
@@ -174,6 +187,7 @@ class ReviewServiceTest {
 
                 reviewResponseDto = reviewService.findReview(seller.getId());
             }
+
             @Test
             @DisplayName("총대 닉네임과 리뷰 항목 별 갯수를 반환한다.")
             void it_returns_seller_nickname_and_reviews_count() {
@@ -184,6 +198,7 @@ class ReviewServiceTest {
                 assertEquals(40, reviewResponseDto.getArrivalSatisfactoryCnt());
             }
         }
+
         @Nested
         @DisplayName("sellerId 값이 유효하지 않은 경우")
         class Context_with_not_exist_sellerId {
@@ -191,6 +206,7 @@ class ReviewServiceTest {
             void setUp() {
                 given(memberRepository.findById(seller.getId())).willReturn(Optional.empty());
             }
+
             @Test
             @DisplayName("해당 유저를 찾을 수 없음 예외를 발생시킨다.")
             void it_returns_not_found_member() {
