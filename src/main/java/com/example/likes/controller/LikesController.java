76ecca701b03fb45a9e774a8bdc729e92c4ad6a5
Kim Jihyun <legojhk@gmail.com>
2024-05-10 02:48:21 +0900

    feat(likes) : base response 삭제

--- src/main/java/com/example/likes/controller/LikesController.java
+++ src/main/java/com/example/likes/controller/LikesController.java
@@ -1,6 +1,5 @@
 package com.example.likes.controller;
 
-import com.example.common.global.BaseResponse;
 import com.example.common.global.PageResponseDto;
 import com.example.likes.dto.LikesResponseDto;
 import com.example.likes.service.LikesService;
@@ -8,7 +7,6 @@ import com.example.product.dto.response.ProductResponseDto;
 import com.example.security.authentication.AuthenticatedMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -20,33 +18,33 @@ public class LikesController {
     private final LikesService likesService;
 
     @PostMapping("/products/{productId}/likes")
-    public ResponseEntity<BaseResponse<LikesResponseDto>> likeProduct(
+    public ResponseEntity<LikesResponseDto> likeProduct(
             @PathVariable Long productId,
             @AuthenticationPrincipal AuthenticatedMember member) {
 
         LikesResponseDto responseDto = likesService.likeProduct(productId, member.getMemberId());
 
-        return ResponseEntity.ok().body(BaseResponse.of(HttpStatus.OK, responseDto));
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @PostMapping("/products/{productId}/unlikes")
-    public ResponseEntity<BaseResponse<LikesResponseDto>> unlikeProduct(
+    public ResponseEntity<LikesResponseDto> unlikeProduct(
             @PathVariable Long productId,
             @AuthenticationPrincipal AuthenticatedMember member) {
 
         LikesResponseDto responseDto = likesService.unlikeProduct(productId, member.getMemberId());
 
-        return ResponseEntity.ok().body(BaseResponse.of(HttpStatus.OK, responseDto));
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @GetMapping("/members/likes")
-    public ResponseEntity<BaseResponse<PageResponseDto<ProductResponseDto>>> getLikedProducts(
+    public ResponseEntity<PageResponseDto<ProductResponseDto>> getLikedProducts(
             @RequestParam("page") int page,
             @RequestParam("size") int size,
             @AuthenticationPrincipal AuthenticatedMember member) {
 
         Page<ProductResponseDto> responseDtoPage = likesService.getLikedProducts(page - 1, size, member.getMemberId());
 
-        return ResponseEntity.ok().body(BaseResponse.of(HttpStatus.OK, PageResponseDto.toResponseDto(responseDtoPage)));
+        return ResponseEntity.ok().body(PageResponseDto.toResponseDto(responseDtoPage));
     }
 }
