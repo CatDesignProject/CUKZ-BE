202fd16722bf252caa777914b10a01686dd1fd13
YerimSW <swyr1016@gmail.com>
2024-05-13 19:49:22 +0900

    other: BaseResponseDto 제거

--- src/main/java/com/example/member/controller/MemberController.java
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.example.member.controller;
 
-import com.example.common.global.BaseResponse;
 import com.example.common.global.PageResponseDto;
 import com.example.member.dto.MemberRegisterRequestDto;
 import com.example.member.dto.MemberResponseDto;
@@ -10,7 +9,6 @@ import com.example.security.authentication.AuthenticatedMember;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,34 +23,30 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/members/register")
-    public ResponseEntity<BaseResponse<String>> registerMember(@RequestBody @Validated MemberRegisterRequestDto memberRegisterRequestDto) {
+    public ResponseEntity<String> registerMember(@RequestBody @Validated MemberRegisterRequestDto memberRegisterRequestDto) {
         memberService.registerMember(memberRegisterRequestDto);
-        return ResponseEntity.ok()
-                .body(BaseResponse.of(HttpStatus.CREATED, "회원가입 완료"));
+        return ResponseEntity.ok().body("회원가입 완료");
     }
 
     @PostMapping("/members/verify-username")
-    public ResponseEntity<BaseResponse<Boolean>> registerMember(@RequestBody VerifyUsernameDto verifyUsernameDto) {
+    public ResponseEntity<Boolean> registerMember(@RequestBody VerifyUsernameDto verifyUsernameDto) {
         boolean isUsernameDuplicated = memberService.isUsernameDuplicated(verifyUsernameDto.getUsername());
-        return ResponseEntity.ok()
-                .body(BaseResponse.of(HttpStatus.OK, isUsernameDuplicated));
+        return ResponseEntity.ok().body(isUsernameDuplicated);
     }
 
     @GetMapping("/members/me")
-    public ResponseEntity<BaseResponse<MemberResponseDto>> infoMember(@AuthenticationPrincipal AuthenticatedMember authenticatedMember) {
+    public ResponseEntity<MemberResponseDto> infoMember(@AuthenticationPrincipal AuthenticatedMember authenticatedMember) {
         AuthenticatedMember member = (AuthenticatedMember) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         MemberResponseDto responseDto = memberService.getMember(member.getMemberId());
 
-        return ResponseEntity.ok()
-                .body(BaseResponse.of(HttpStatus.OK, responseDto));
+        return ResponseEntity.ok().body(responseDto);
     }
 
     @GetMapping("/admin/members")
-    public ResponseEntity<BaseResponse> findAllMembers(@RequestParam(defaultValue = "1", name = "page") int page,
+    public ResponseEntity<PageResponseDto> findAllMembers(@RequestParam(defaultValue = "1", name = "page") int page,
                                                        @RequestParam(defaultValue = "10", name = "size") int size) {
         Page<MemberResponseDto> memberResponseDtos = memberService.getAllMembers(page - 1, size);
 
-        return ResponseEntity.ok()
-                .body(BaseResponse.of(HttpStatus.OK, PageResponseDto.toResponseDto(memberResponseDtos)));
+        return ResponseEntity.ok().body(PageResponseDto.toResponseDto(memberResponseDtos));
     }
 }
