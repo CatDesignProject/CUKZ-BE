5ff9ca4c2ed953fd672c07125181a56b17cf4451
YerimSW <swyr1016@gmail.com>
2024-05-15 12:55:58 +0900

    other: BaseResponse 제거 및 review api 비회원 조회 허용

--- src/main/java/com/example/security/authentication/CustomLogoutSuccessHandler.java
+++ src/main/java/com/example/security/authentication/CustomLogoutSuccessHandler.java
@@ -1,11 +1,9 @@
 package com.example.security.authentication;
 
-import com.example.common.global.BaseResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -18,12 +16,10 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        BaseResponse<Boolean> logoutResponseDto = BaseResponse.of(HttpStatus.OK, true);
-
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
 
-        response.getWriter().write(objectMapper.writeValueAsString(logoutResponseDto));
+        response.getWriter().write(objectMapper.writeValueAsString(true));
         response.flushBuffer();
     }
 }
