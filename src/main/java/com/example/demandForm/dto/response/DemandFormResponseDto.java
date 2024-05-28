f4f4bc8226351bfe2f811b5eac8fc01539612093
Kim Jihyun <legojhk@gmail.com>
2024-05-28 14:58:25 +0900

    refactor(demandForm) : response dto 항목 추가

--- src/main/java/com/example/demandForm/dto/response/DemandFormResponseDto.java
+++ src/main/java/com/example/demandForm/dto/response/DemandFormResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.demandForm.dto.response;
 
 import com.example.demandForm.entity.DemandForm;
+import com.example.product.enums.SaleStatus;
+import com.example.product_image.entity.ProductImage;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,11 +17,20 @@ import java.util.List;
 public class DemandFormResponseDto {
 
     private Long id;
-    private Long productId;
     private Long memberId;
+    private String email;
+    private Long productId;
+    private String productName;
+    private int price;
+    private SaleStatus status;
+    private List<String> imageUrlList;
     private List<FormOptionResponseDto> optionList;
 
     public static DemandFormResponseDto toResponseDto(DemandForm demandForm) {
+
+        List<String> imageUrlList = demandForm.getProduct().getProductImages().stream()
+                .map(ProductImage::getImageUrl)
+                .toList();
 
         List<FormOptionResponseDto> optionList = demandForm.getDemandOptionList().stream()
                 .map(FormOptionResponseDto::toResponseDto)
@@ -27,8 +38,13 @@ public class DemandFormResponseDto {
 
         return DemandFormResponseDto.builder()
                 .id(demandForm.getId())
-                .productId(demandForm.getProduct().getId())
                 .memberId(demandForm.getMemberId())
+                .email(demandForm.getEmail())
+                .productId(demandForm.getProduct().getId())
+                .productName(demandForm.getProduct().getName())
+                .price(demandForm.getProduct().getPrice())
+                .status(demandForm.getProduct().getStatus())
+                .imageUrlList(imageUrlList)
                 .optionList(optionList)
                 .build();
     }
